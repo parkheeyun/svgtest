64c83bdd5f89e28dca3b50c7aecc2c53c86ae936
unknown <heeyun8662@naver.com>
2020-07-28 14:41:06 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from './svgs/login';
+
+function App() {
+  return (
+    <div className="App">
+        hello world!
+        <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/svgs/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Login = () => {
+
+
+
+    return (
+        <p>hello login</p>
+    )
+
+
+
+}
+
+export default Login
